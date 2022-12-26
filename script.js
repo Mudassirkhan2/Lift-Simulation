4df30002f34f016f0f04e8596479417e5c4661ae
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2022-12-26 19:23:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,132 @@
-let inputNumber = document.getElementById("inputNumber");
-const form = document.getElementById("form");
-const container = document.querySelector(".container");
-const lift = document.querySelector(".lift");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  font-size: 16px;
+}
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  inputNumber = inputNumber.value;
-  if (inputNumber > 0 && inputNumber < 100) {
-    container.classList.remove("hidden");
-    form.classList.add("hidden");
+body {
+  max-width: 1200px;
+  width: 90%;
+  margin: 0 auto;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #243763;
+}
 
-    for (let index = 1; index < inputNumber; index++) {
-      container.insertAdjacentHTML(
-        "afterbegin",
-        ` <div class="floor-container">
-   <div class="btn-container">
-     <span><button class="btn-up">Up</button></span>
+.container {
+  width: 600px;
+  margin: 0 auto;
+}
+.horizontal-flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+}
+hr {
+  width: 85%;
+  border: 1px solid black;
+}
+label,
+p {
+  font-weight: bold;
+}
+label {
+  color: #fff6bd;
+}
+.lift {
+  display: inline-block;
+  width: 3rem;
+  height: 6rem;
+  background: dodgerblue;
+  position: relative;
+  bottom: -1rem;
+  left: 1rem;
+  overflow: hidden;
+  transition: transform 2.5s ease;
+}
+.move-up {
+  transform: translateY(-100%);
+}
 
-     <span><button class="btn-down">Down</button></span>
-   </div>
-   <div class="horizontal-flex">
-     <hr />
-     <p>Floor ${index}</p>
-   </div>
- </div>`
-      );
-    }
-    container.insertAdjacentHTML(
-      "afterbegin",
-      ` <div class="floor-container">
- <div class="btn-container">
+.move-down {
+  transform: translateY(100%);
+}
+.btn-up,
+.btn-down {
+  position: relative;
+  top: -1rem;
+  padding: 0.25rem 0.7rem;
+  background-color: forestgreen;
+  border: none;
+  border-radius: 5px;
+  margin-top: 2rem;
+  cursor: pointer;
+}
+#btn-up-first {
+  margin-top: 7.56rem;
+}
+.btn-down {
+  background-color: yellow;
+  padding: 0.25rem 0.25rem;
+}
+.btn-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+.hidden {
+  display: none;
+}
+h1 {
+  color: cyan;
+  font-size: 2rem;
+  font-weight: bolder;
+  margin-bottom: 2rem;
+  font-family: Baumans;
+  text-shadow: 0px 0px 2px rgb(255, 255, 255), 0px 0px 4px rgb(255, 255, 255),
+    0px 0px 8px rgb(255, 255, 255), 0px 0px 12px rgb(73, 255, 24),
+    0px 0px 16px rgb(73, 255, 24), 0px 0px 20px rgb(73, 255, 24),
+    0px 0px 24px rgb(73, 255, 24), 0px 0px 28px rgb(73, 255, 24);
+}
 
-   <span><button class="btn-down">Down</button></span>
- </div>
- <div class="horizontal-flex">
-   <hr />
-   <p>Floor ${inputNumber}</p>
- </div>
-</div>`
-    );
+input {
+  width: 20%;
+  border-radius: 5px;
+}
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  margin: 0;
+}
+input[type="number"]:focus {
+  outline: none;
+}
+.btn-up:active,
+.btn-down:active {
+  background: cyan;
+}
 
-    // events for Button up
-
-    const btnUp = [...document.querySelectorAll(".btn-up")];
-    let count = 0;
-    btnUp.reverse();
-    btnUp.map(function (block, index) {
-      block.addEventListener("click", function () {
-        let countup = index * 2;
-        let translateValue = -10 * index - 0.4;
-        let transitionTime = Math.abs(countup - count);
-        lift.style.transition = `transform ${transitionTime}s ease`;
-        lift.style.transform = `translateY(${translateValue}rem)`;
-        return (count = countup);
-      });
-    });
-
-    // events for Button down
-    const btnDown = [...document.querySelectorAll(".btn-down")];
-    btnDown.reverse();
-    btnDown.map(function (block, index) {
-      block.addEventListener("click", function () {
-        let countup = (index + 1) * 2;
-        let translateValue = -10 * index - 0.4 - 10;
-        let transitionTime = Math.abs(countup - count);
-        lift.style.transition = `transform ${transitionTime}s ease`;
-        lift.style.transform = `translateY(${translateValue}rem)`;
-        return (count = countup);
-      });
-    });
-  } else {
-    alert("Please Enter a Value between 0 and 100");
-    location.reload();
+@media (max-width: 475px) {
+  h1 {
+    font-size: 1rem;
+    margin-bottom: 2rem;
   }
-});
+  .container {
+    width: 350px;
+    margin: 0 auto;
+    min-width: 90%;
+  }
+  hr {
+    width: 75%;
+  }
+}
