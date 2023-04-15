0a9e471b29fa0ee8ff38392dbde72db196f07361
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-04-15 18:16:34 +0530

    solved bug of multiple lifts

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const openingPage = document.getElementById("openingPage");
 const generate = document.getElementById("generate");
 const container = document.querySelector(".container") ;
 const firstLift = document.querySelector(".first-lift") ;
-const lift = document.querySelector(".lift");
-const leftDoor = document.querySelector(".left-door");
-const rightDoor = document.querySelector(".right-door");
+
 
 generate.addEventListener("click", (e) => {
   e.preventDefault();
@@ -100,6 +98,38 @@ generate.addEventListener("click", (e) => {
       }
   
     }
+
+
+    // const lift = document.querySelector(".lift");
+    const lift = [...document.querySelectorAll(".lift")];
+    const leftDoor = document.querySelectorAll(".left-door");
+    const rightDoor = document.querySelectorAll(".right-door");  
+    let indexLift =1
+    let liftanimationindex=0
+    function liftAnimation(block, buttons,liftanimationindex,indexLift) {
+      console.log(liftanimationindex)
+      // opens the doors
+      leftDoor[liftanimationindex].classList.add("left-move-open");
+
+      rightDoor[liftanimationindex].classList.add("right-move-open");
+    
+      // to close the doors
+      leftDoor[liftanimationindex].addEventListener("transitionend", () => {
+        leftDoor[liftanimationindex].classList.add("left-move-close");
+
+        rightDoor[liftanimationindex].classList.add("right-move-close");
+    
+        //  to remove all the classes after transition
+        setTimeout(() => {
+          leftDoor[liftanimationindex].classList.remove("left-move-close");
+          rightDoor[liftanimationindex].classList.remove("right-move-close");
+          leftDoor[liftanimationindex].classList.remove("left-move-open");
+          rightDoor[liftanimationindex].classList.remove("right-move-open");
+            indexLift =0
+            console.log("transition end index " ,indexLift)
+        }, 2500);
+      });
+    }
     // events for Button up
     const btnUp = [...document.querySelectorAll(".btn-up")];
     let count = 0;
@@ -113,29 +143,42 @@ generate.addEventListener("click", (e) => {
         //To Check that user not clicked the same floor
         if (count != countup) {
           let translateValue = -10 * index - 1;
-
-          // disable buttons
-          buttons.forEach((button) => {
-            button.setAttribute("disabled", true);
-          });
-
           let transitionTime = Math.abs(countup - count);
-          lift.style.transform = `translateY(${translateValue}rem)`;
-          lift.style.transition = `transform ${transitionTime}s ease  `;
+          console.log(indexLift)
+          lift[indexLift].style.transform = `translateY(${translateValue}rem)`;
+          lift[indexLift].style.transition = `transform ${transitionTime}s ease`;
+
+          function transitionRunListener() {
+            console.log("transition start")
+            console.log(indexLift)
+            if(indexLift < inputFloor-1){
+              liftanimationindex = indexLift
+              indexLift = indexLift + 1
+            }
+            else{
+              console.log("hii")
+              liftanimationindex = indexLift
+              indexLift=0
+            }
+            console.log("transition start index ", indexLift)
+            this.removeEventListener("transitionrun", transitionRunListener);
+          }
           // transition finished event
-          lift.addEventListener("transitionend", () => {
+          function transitionEndListener() {
             block.classList.remove("btn-clicked");
-            liftAnimation(block, buttons);
-          });
+            liftAnimation(block, buttons, liftanimationindex, indexLift);
+            console.log("transitionend")
+            this.removeEventListener("transitionend", transitionEndListener);
+          }
+          
+          lift[indexLift].addEventListener("transitionrun", transitionRunListener);
+          lift[indexLift].addEventListener("transitionend", transitionEndListener);
+          
           return (count = countup);
         }
         //if same floor
         else {
           block.classList.remove("btn-clicked");
-          // disable buttons
-          buttons.forEach((button) => {
-            button.setAttribute("disabled", true);
-          });
           liftAnimation(block, buttons);
         }
       });
@@ -155,30 +198,42 @@ generate.addEventListener("click", (e) => {
         if (count != countup) {
           let translateValue = -10 * index - 11; //added 10 extra because down is starting from 1st floor
 
-          // disable buttons
-          buttons.forEach((button) => {
-            button.setAttribute("disabled", true);
-          });
+         
 
           let transitionTime = Math.abs(countup - count);
-          lift.style.transform = `translateY(${translateValue}rem)`;
-          lift.style.transition = `transform  ${transitionTime}s ease  `;
+          lift[indexLift].style.transform = `translateY(${translateValue}rem)`;
+          lift[indexLift].style.transition = `transform  ${transitionTime}s ease  `;
 
+          function transitionRunListener() {
+            console.log("transition start")
+            console.log(indexLift)
+            if(indexLift < inputFloor-1){
+              liftanimationindex = indexLift
+              indexLift = indexLift + 1
+            }
+            else{
+              console.log("hii")
+              liftanimationindex = indexLift
+              indexLift=0
+            }
+            console.log("transition start index ", indexLift)
+            this.removeEventListener("transitionrun", transitionRunListener);
+          }
           // transition finished event
-          lift.addEventListener("transitionend", () => {
+          function transitionEndListener() {
             block.classList.remove("btn-clicked");
-
-            liftAnimation(block, buttons);
-          });
+            liftAnimation(block, buttons, liftanimationindex, indexLift);
+            console.log("transitionend")
+            this.removeEventListener("transitionend", transitionEndListener);
+          }
+          
+          lift[indexLift].addEventListener("transitionrun", transitionRunListener);
+          lift[indexLift].addEventListener("transitionend", transitionEndListener);
           return (count = countup);
         }
         //if same floor
         else {
           block.classList.remove("btn-clicked");
-          // disable buttons
-          buttons.forEach((button) => {
-            button.setAttribute("disabled", true);
-          });
           liftAnimation(block, buttons);
         }
       });
@@ -187,27 +242,7 @@ generate.addEventListener("click", (e) => {
     alert("Please Enter a Floors Value between 0 and 100");
     location.reload();
   }
+  
 });
 
-function liftAnimation(block, buttons) {
-  // opens the doors
-  leftDoor.classList.add("left-move-open");
-  rightDoor.classList.add("right-move-open");
 
-  // to close the doors
-  leftDoor.addEventListener("transitionend", () => {
-    leftDoor.classList.add("left-move-close");
-    rightDoor.classList.add("right-move-close");
-
-    //  to remove all the classes after transition
-    setTimeout(() => {
-      leftDoor.classList.remove("left-move-close");
-      rightDoor.classList.remove("right-move-close");
-      leftDoor.classList.remove("left-move-open");
-      rightDoor.classList.remove("right-move-open");
-      buttons.forEach((button) => {
-        button.removeAttribute("disabled");
-      });
-    }, 2500);
-  });
-}
